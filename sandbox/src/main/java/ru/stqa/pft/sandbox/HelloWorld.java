d370cdc0a78bb5d2fceac1e0a19c6542ca7dcc2e
o99l <54982046+o99l@users.noreply.github.com>
2019-09-12 15:00:37 +0300

    Добавлены Классы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -1,7 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("World");
+        hello("User");
+        hello("Kostya");
+
+        Square s = new Square(5);
+        Rectangle r = new Rectangle(4,6);
+
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
     }
 }
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public double area(){
+        return this.l * this.l;
+    }
+
+    public Square(double l){
+        this.l=l;
+        }
+}
